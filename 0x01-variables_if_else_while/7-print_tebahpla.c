3cd34b76814b75d48bc1ec2a06fd8b6fa161e99a
PertuniaZulu <pertuniazulu0@gmail.com>
2023-02-17 23:15:24 +0200

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+i#include <stdio.h>
+/**
+ * main - main block
+ * Description: this program prints lowercases alphabet in reverse order
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

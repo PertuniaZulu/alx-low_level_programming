7ea3e1cbe4e9bba0e184498f7bedf37b93d0f9bf
PertuniaZulu <pertuniazulu0@gmail.com>
2023-02-21 17:22:22 +0200

    added 2-print_alphabet_x10.c again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,3 @@
-#
 #include "main.h"
 
 /**
@@ -10,13 +9,16 @@
 
 void print_alphabet_x10(void)
 {
-	int c;
-	char i;
-
-	for (c = 'a'; i <= 'z'; c++)
-	{
-		for (i = 'a'; c <= 'z'; i++)
-			_putchar(i);
-		_putchar('\n');
-	}
+int b = 0;
+while (b < 10)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+_putchar('\n');
+b++;
+}
 }

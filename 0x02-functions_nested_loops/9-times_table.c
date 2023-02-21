1623ce803243bf8976c81c0b07d4895b971c933a
PertuniaZulu <pertuniazulu0@gmail.com>
2023-02-21 19:45:24 +0200

    added 9-times_table.c333

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table -  a function that prints the 9 times table, starting with 0
+ * times_table - a function that prints the 9 times table, starting with 0
  * a = row, b = column, c = current result
  * Return: times table
  * add extra space past single digit
@@ -8,32 +8,32 @@
 void times_table(void)
 {
 int a, b, c;
-
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-	for (b = 1; b <= 9; b++)
-	{
-		c = (a * b);
-	if ((c / 10) > 0)
-	{
-		_putchar((b / 10) + '0');
-	}
-	else
-	{
-		_putchar(' ');
-	}
-		_putchar((c % 10) + '0');
-
-	if (b < 9)
-	{
-		_putchar(',');
-	      _putchar(' ');
-	}
-	}
-	_putchar('\n');
-	}
-
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+k = b * a;
+if (b  == 0)
+{
+_putchar(c + '0');
 }
+if (c < 10 && j != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+
+

995be85bcfa7118665bc8e3772c9d70c090b55be
PertuniaZulu <pertuniazulu0@gmail.com>
2023-03-02 20:01:25 +0200

    added 4-rev_array.c666

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,14 +7,14 @@
  */
 void reverse_array(int *a, int n)
 {
-	int tmp, j, c = 0;
+	int tmp, j, c;
 
 	j = n - 1;
 
 	for (c = 0; c < n / 2; c++)
 	{
 		tmp = a[c];
-		a[c++] = a[j];
+		a[c] = a[j];
 		a[j--] = tmp;
 	}
 }

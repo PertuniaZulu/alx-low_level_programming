02ea57c307074e2fcfd11748bc460f0cfb10cda1
PertuniaZulu <pertuniazulu0@gmail.com>
2023-03-02 19:40:46 +0200

    added 3-strcmp.cuuuu

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int c = 0, n = 0;
+	int c = 0, n;
 
-	while (n == 0)
+	while (s1[c] == s2[c] && s1[c] != '\0')
 	{
-		if ((*(s1 + c) == '\0') && (*(s2 + c) == '\0'))
-			break;
-		op = *(s1 + c) - *(s2 + c);
 		c++;
 	}
-
+	n = s1[c] - s2[c];
 	return (n);
 }
 
